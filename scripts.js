04664ae98b5d6c94019925783b017f2a33dd6347
mmarmol88 <mindysoto88@gmail.com>
2020-01-07 19:54:44 -0500

    created question constructor, need to re-work code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,34 @@
 console.log('It is a great day to code');
 
+//Create the questions using OOP the constructor method
+// class Americas {
+//   constructor(pointValue, question, answerOne, answerTwo, correctAnswer) {
+//     this.pointValue = pointValue;
+//     this.question = question;
+//     this.answerOne = answerOne;
+//     this.answerTwo = answerTwo;
+//     this.correctAnswer = correctAnswer;
+//   }
+// }
+
+// const americasFifty = new Americas(
+//   50,
+//   'Have you heard the latest buzz? The Mayans domesticated this native insect',
+//   'fishing worm',
+//   'silkworm',
+//   'bee'
+// );
+// console.log(americasFifty);
+// const americasHundred = new Americas(
+//   100,
+//   'In 1513 this Spaniard made the first recorded European exploration of what is now the U.S. mainland',
+//   'Christopher Columbus',
+//   'Simon Bolivar',
+//   'Ponce De Leon'
+// );
+// console.log(americasHundred);
+// console.log(Americas);
+
 //Add event listener to question options
 const questionOptions = document.querySelector('.quiz-options');
 console.log(questionOptions);
@@ -9,16 +38,17 @@ questionOptions.addEventListener('click', questionHandler);
 function questionHandler(evt) {
   evt.preventDefault();
   console.log(evt);
-  //Grab the quiz area to unhide it
-  const quizArea = document.querySelector('.quiz-area');
+  //provide the question and answers into the question-area
+  // const questionArea = document.querySelector('.question');
+  // questionArea.innerText = Americas[0].question;
+  //Grab the quiz area to un-hide it
+  const quizArea = document.querySelector('.americas-fifty');
   quizArea.style.display = 'inline';
-
-  // if (evt.target.className === 'grid') {
-  // }
+  handleAnswer();
 }
 
 //add event listener to question buttons
-const answerButton = document.querySelector('.question-area');
+const answerButton = document.querySelector('.answer-area');
 console.log(answerButton);
 answerButton.addEventListener('click', handleAnswer);
 
@@ -31,6 +61,9 @@ function handleAnswer(evt) {
     evt.target.style.backgroundColor = 'green';
     evt.target.style.color = 'white';
     evt.target.innerText = 'You are correct';
+    //Attempt to hide the question area
+    //Add function to run once the user has picked an answer
+    buttonDisabler();
   } else if (
     evt.target.nodeName === 'BUTTON' &&
     evt.target.dataset.key !== 'xy'
@@ -39,10 +72,9 @@ function handleAnswer(evt) {
     evt.target.style.backgroundColor = 'red';
     evt.target.style.color = 'white';
     evt.target.innerText = "You'll get it next time";
+    buttonDisabler();
   }
-
-  //Add function to run once the user has picked an answer
-  buttonDisabler();
+  //Attempt to hide the question area
 }
 
 // grab all user response buttons
@@ -50,19 +82,11 @@ function handleAnswer(evt) {
 
 //create a function to disable the user response buttons
 function buttonDisabler(evt) {
-  evt.preventDefault();
-  console.log(evt.target);
   if (
     evt.target.className === 'disable-button' &&
-    evt.target.disabled === false
+    evt.target.target.disabled === false
   ) {
     console.log(evt.target);
     evt.target.disabled = true;
   }
-
-  // console.log(evt.target);
-  // console.log(buttonA);
-  // if (buttonA.disabled === false) {
-  //   buttonA.disabled = true;
-  // }
 }
---
+++ scripts2.js
@@ -0,0 +1,10 @@
+//Create a question constructor that has subclasses of (Americas, Asia, and Europe);
+//Add event listener to my point-value buttons
+//Connect the point-value buttons from my question selection area to be associated with appropriate subclass of questions
+//Append the selected point-value question and related answers to the question-area
+//Add event-listener to the question area for answer selection
+
+//write if/else statement that checks if the answer is correct or wrong and adds a button that notifies user if they are correct
+//return the point-value to a score array
+//check if score array sum = 1000 points
+//notify player that they won
